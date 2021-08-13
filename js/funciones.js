c19264a46073e8e869229846ec59454438f09be0
Meta-Willi <59988459+Meta-Willi@users.noreply.github.com>
2021-08-13 09:39:55 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,35 @@
+function captura() {
+    var nombre=document.getElementById("nomest").value;
+    var apellido=document.getElementById("apell").value;
+    var pais=document.getElementById("pais").value;
+    var email=document.getElementById("email").value;
+    var genero=document.getElementById("genero").value;
+    var naci=document.getElementById("nacimiento").value;
+    var paisres=document.getElementById("paisres").value;
+    var depares=document.getElementById("depares").value;
+    var telcasa=document.getElementById("telcasa").value;
+    var telcel=document.getElementById("telcelular").value;
+    var domici=document.getElementById("domicilio").value;
+
+    document.write(nombre);
+    document.write('<br>');
+    document.write(apellido);
+    document.write('<br>');
+    document.write(pais);
+    document.write('<br>');
+    document.write(email);
+    document.write('<br>');
+    document.write(genero);
+    document.write('<br>');
+    document.write(naci);
+    document.write('<br>');
+    document.write(paisres);
+    document.write('<br>');
+    document.write(depares);
+    document.write('<br>');
+    document.write(telcasa);        
+    document.write('<br>');
+    document.write(telcel);
+    document.write('<br>');
+    document.write(domici);
+} 
